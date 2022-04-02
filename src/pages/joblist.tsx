8e560da94f8b7c0abed70a61a933e293466a9d24
NelsonWang <wangnelson2@gmail.com>
2022-04-02 18:12:14 -0400

    Joblist-test

---
+++ src/pages/joblist.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return <>
+  <h1>Job List</h1>
+  <table>
+      <th>The JOb list</th>
+      <tr></tr>
+  </table>
+  </>;
+};
+
+export default IndexPage;
