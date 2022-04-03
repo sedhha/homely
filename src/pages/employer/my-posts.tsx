e1dee9ffedeada4dddb72f37f99fbb9b62623cca
NelsonWang <wangnelson2@gmail.com>
2022-04-03 01:20:45 -0400

    joblist

--- src/pages/employer/my-posts.tsx
+++ src/pages/employer/my-posts.tsx
@@ -5,29 +5,31 @@ const MyPosts: NextPage = () => {
   return (
     <>
       <Header />
-      <h1>My Job Posts</h1>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>Job Title</th>
-            <th>Description</th>
-            <th>Deadline</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>Sample Job</th>
-            <th>Sample Description</th>
-            <th>Tomorrow</th>
-            <th>
-              <button className="btn btn-primary">Edit</button>
-            </th>
-            <th>
-              <button className="btn btn-primary">Check Applications</button>
-            </th>
-          </tr>
-        </tbody>
-      </table>
+      <div className="container">
+        <h1>My Job Posts</h1>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Job Title</th>
+              <th>Description</th>
+              <th>Deadline</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Sample Job</th>
+              <th>Sample Description</th>
+              <th>Tomorrow</th>
+              <th>
+                <button className="btn btn-primary">Edit</button>
+              </th>
+              <th>
+                <button className="btn btn-primary">Check Applications</button>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
--- src/pages/joblist.tsx
+++ src/pages/joblist.tsx
@@ -1,31 +1,32 @@
 import type { NextPage } from "next";
-import Header from "../components/header"
-
+import Header from "../components/header";
 
 const IndexPage: NextPage = () => {
   return (
     <>
-    <Header />
+      <Header />
       <h1>Job List</h1>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>Job Title</th>
-            <th>Description</th>
-            <th>Deadline</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>Sample Job</th>
-            <th>Sample Description</th>
-            <th>Tomorrow</th>
-            <th>
-              <button className="btn btn-primary">Apply</button>
-            </th>
-          </tr>
-        </tbody>
-      </table>
+      <div className="container-lg">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Job Title</th>
+              <th>Description</th>
+              <th>Deadline</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Sample Job</th>
+              <th>Sample Description</th>
+              <th>Tomorrow</th>
+              <th>
+                <button className="btn btn-primary">Apply</button>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
