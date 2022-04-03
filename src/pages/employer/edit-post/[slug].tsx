eb2f2cb4c9083f3363eae4d5eef3c2855db7c8a9
sedhha <activity.schoolsh2@gmail.com>
2022-04-03 21:08:41 +0530

    Warning Commit

--- src/pages/employer/edit-post/[slug].tsx
+++ src/pages/employer/edit-post/[slug].tsx
@@ -1,38 +1,40 @@
-import type { NextPage } from "next";
-import DatePicker from "react-datepicker";
+import type { NextPage } from 'next';
+import DatePicker from 'react-datepicker';
 
 const EditPost: NextPage = () => {
-
-  const deadline = 1649083773000; 
+  const deadline = 1649083773000;
   return (
     <>
       <h1>Edit Job Description</h1>
       <form>
-        <div className="form-group">
+        <div className='form-group'>
           <label>Job Title</label>
-          <input type="text" value={"Sample Job"} className="form-control" />
+          <input type='text' value={'Sample Job'} className='form-control' />
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <label>Description</label>
-          <input type="text" value={"Sample Description"}  className="form-control" />
+          <input
+            type='text'
+            value={'Sample Description'}
+            className='form-control'
+          />
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <label>Deadline</label>
-          <input type="text" className="form-control" value={"Tomorrow"} />
+          <input type='text' className='form-control' value={'Tomorrow'} />
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <label>Deadline</label>
-          <input type="text" className="form-control" value={"10"} />
+          <input type='text' className='form-control' value={'10'} />
         </div>
-        <div className="form-group">
-            <label>Deadline</label>
-            <DatePicker
-              selected={deadline}
-              onChange={(date: Date) => {
-              }}
-            />
-          </div>
-        <button type="submit" className="btn btn-primary">
+        <div className='form-group'>
+          <label>Deadline</label>
+          <DatePicker
+            selected={new Date(deadline)}
+            onChange={(date: Date) => {}}
+          />
+        </div>
+        <button type='submit' className='btn btn-primary'>
           Submit
         </button>
       </form>
