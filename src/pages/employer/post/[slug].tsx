84cef6a606d32b2622608d8148b2a07bcf6ef8c7
NelsonWang <wangnelson2@gmail.com>
2022-04-03 11:27:05 -0400

    merge

--- src/pages/employer/post/[slug].tsx
+++ src/pages/employer/post/[slug].tsx
@@ -3,29 +3,7 @@ import type { NextPage } from "next";
 const Post: NextPage = () => {
   return (
     <>
-      <h1>Edit Job Description</h1>
-      <form>
-        <div className="form-group">
-          <label>Job Title</label>
-          <input type="text" value={"Sample Job"} className="form-control" />
-        </div>
-        <div className="form-group">
-          <label>Description</label>
-          <input
-            type="text"
-            value={"Sample Description"}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label>Deadline</label>
-          <input type="text" value={"4.4.2022"} className="form-control" />
-        </div>
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      <h1>Check Aplications Details</h1>
     </>
   );
 };
