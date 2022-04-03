ee9ef1396a5f3275c03023ca2b14cd9e6c963455
NelsonWang <wangnelson2@gmail.com>
2022-04-03 12:14:59 -0400

    items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
+
+import Header from "src/components/header";
+import { Text } from "@chakra-ui/react";
+
+
 
 const IndexPage: NextPage = () => {
-  return <div>Welcome Page</div>;
+  return (
+    <>
+      <Header />
+      <br />
+      <div className="container">
+        <Text fontSize="6xl">Welcome Page</Text>
+      </div>
+    </>
+  );
 };
 
 export default IndexPage;
