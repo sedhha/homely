4cada9e9a087571736e8d5562cefc061de2b15a2
NelsonWang <wangnelson2@gmail.com>
2022-04-02 21:19:27 -0400

    slugs

--- src/pages/employer/my-posts.tsx
+++ src/pages/employer/my-posts.tsx
@@ -3,7 +3,29 @@ import type { NextPage } from "next";
 const MyPosts: NextPage = () => {
   return (
     <>
-      <h1>My Posts</h1>
+      <h1>My Job Posts</h1>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Job Title</th>
+            <th>Description</th>
+            <th>Deadline</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Sample Job</th>
+            <th>Sample Description</th>
+            <th>Tomorrow</th>
+            <th>
+              <button className="btn btn-primary">Edit</button>
+            </th>
+            <th>
+              <button className="btn btn-primary">Check Applications</button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
---
+++ src/pages/employer/post/[slug].tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from "next";
+
+const Post: NextPage = () => {
+  return (
+    <>
+      <h1>Edit Job Description</h1>
+      <form>
+        <div className="form-group">
+          <label>Job Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Description</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Deadline</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Post;
