7c430dac59484f94a97a49a2ef336cbcac26ee1c
NelsonWang <wangnelson2@gmail.com>
2022-04-02 22:49:37 -0400

    header | stuff

--- src/pages/employer/create-jobs.tsx
+++ src/pages/employer/create-jobs.tsx
@@ -1,12 +1,31 @@
 import type { NextPage } from "next";
 import Header from "src/components/header";
 
-
 const EditJob: NextPage = () => {
-  return <>
-  <Header />
-  <h1>Create Job Post</h1>
-  </>;
+  return (
+    <>
+      <Header />
+      <h1>Create Job</h1>
+      <form>
+        <div className="form-group">
+          <label>Job Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Description</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Deadline</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Post Job
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default EditJob;
